2aec76d496a0ee545c6fc212c6e44e8b80395060
Wu Jianmin <a601767890@sina.com>
2017-08-07 11:54:30 +0800

    change urls

--- configure.py
+++ configure.py
@@ -2,23 +2,6 @@
 urls = [
 		"https://www.iconfinder.com/iconsets/3d-printing-6",
 		"https://www.iconfinder.com/iconsets/ecology-33",
-		"https://www.iconfinder.com/iconsets/lined-business-people",
-		"https://www.iconfinder.com/iconsets/3d-printing-4",
-		"https://www.iconfinder.com/iconsets/finance-business-2-2",
-		"https://www.iconfinder.com/iconsets/industry-oil-gas-power-colours",
-		"https://www.iconfinder.com/iconsets/profession-filled-line",
-		"https://www.iconfinder.com/iconsets/lined-documents",
-		"https://www.iconfinder.com/iconsets/aiga-symbol-signs",
-		"https://www.iconfinder.com/iconsets/news-and-magazine",
-		"https://www.iconfinder.com/iconsets/very-basic-android-l-lollipop",
-		"https://www.iconfinder.com/iconsets/startup-17",
-		"https://www.iconfinder.com/iconsets/business-charts-and-diagrams-colored",
-		"https://www.iconfinder.com/iconsets/healthy-life-1",
-		"https://www.iconfinder.com/iconsets/robotics-line-3",
-		"https://www.iconfinder.com/iconsets/business-charts-and-diagrams-colored-2",
-		"https://www.iconfinder.com/iconsets/beauty-cultural-symbols-spa-and-yoga",
-		"https://www.iconfinder.com/iconsets/discount-1",
-		"https://www.iconfinder.com/iconsets/virtual-reality-15",
 		]
 		
 sizes = [48,64,128,256,512]  # 可选的图片尺寸
